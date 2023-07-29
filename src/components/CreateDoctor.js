62ef51268146191e3cc405fb0d917e72af5a2610
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-29 17:15:10 +0100

    import all necesarry function to create components

--- src/components/CreateDoctor.js
+++ src/components/CreateDoctor.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDoctors, addDoctor } from '../redux/slices/doctors'
+
 
 function CreateDoctor() {
   return (
