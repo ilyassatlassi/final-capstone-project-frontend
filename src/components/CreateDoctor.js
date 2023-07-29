cd60a593c6df344ed2d35e463381a91a42efad60
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-29 17:23:18 +0100

    Add state for doctors

--- src/components/CreateDoctor.js
+++ src/components/CreateDoctor.js
@@ -4,6 +4,21 @@ import { fetchDoctors, addDoctor } from '../redux/slices/doctors'
 
 
 function CreateDoctor() {
+    const dispatch = useDispatch();
+	const { doctor } = useSelector((state) => state.doctors);
+	const [doctorData, setDoctorData] = useState({
+		image: '',
+		name: '',
+		specialization: '',
+		consultationFee: '',
+		hospital: '',
+        availability: '',
+        description: '',
+        facebook: '',
+        twitter:'',
+        instagram:'',
+	});
+
   return (
     <div>CreateDoctor</div>
   )
