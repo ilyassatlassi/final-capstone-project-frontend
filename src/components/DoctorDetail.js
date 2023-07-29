7ed06c42cd5bb3e66a28d2a54d274b6c84cf0541
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-29 17:05:10 +0100

    Create useffect for Doctor id

--- src/components/DoctorDetail.js
+++ src/components/DoctorDetail.js
@@ -7,13 +7,16 @@ import { useParams, Link } from 'react-router-dom';
 
 function DoctorDetail() {
   const dispatch = useDispatch();
+  const { Doctor } = useSelector((state) => state.doctors);
+  const { id } = useParams();
 
   useEffect (()=>{
-
-  })
+    dispatch(fetchDoctors(id));
+  },[id])
 
   return (
-    <div className="text-red-500 shadow flex flex-col lg:flex-row items-center justify-center w-screen h-screen">DoctorDetail</div>
+    <div className="text-red-500 shadow flex flex-col lg:flex-row items-center justify-center w-screen h-screen">DoctorDetail
+    </div>
   )
 }
 
