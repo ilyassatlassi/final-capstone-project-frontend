e1ec22ee42c6a289570e5e894e54e4de098c0174
SabiMantock <sabimantock@gmail.com>
2023-07-31 12:48:27 +0000

    feat: add link to h1 tag

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,14 +5,14 @@ const SideNav = () => (
   <div className=" w-full min-w-[375px] border-r h-screen flex flex-col justify-between py-5 pl-5">
     <div>
       <div className="mb-20 px-5">
-        <h1>DocCare</h1>
+        <h1><a href="/"> DocCare</a></h1>
       </div>
       <ul>
         {NAVLINKS.map(({ name, route }) => (
           <li key={name}>
             <a
               href={route}
-              className="block py-2 px-5 text-gray-600 hover:text-white hover:bg-[#97BF0E] active:bg-blue-600 transition-colors duration-300"
+              className="block py-2 px-5 text-gray-600 hover:text-white hover:bg-[#97BF0E] transition-colors duration-300"
             >
               {name}
             </a>
