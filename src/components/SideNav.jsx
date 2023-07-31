488dde621e0c054c694021e181201d8c4080da13
SabiMantock <sabimantock@gmail.com>
2023-07-31 18:31:37 +0000

    add Nav links

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { NAVLINKS, date } from '../constants/constants';
 
 const SideNav = () => (
-  <div className=" w-full min-w-[375px] border-r h-screen flex flex-col justify-between py-5 pl-5">
+  <div className="w-full h-full flex flex-col justify-between pt-20 lg:py-5 pl-10 lg:pl-5">
     <div>
       <div className="mb-20 px-5">
-        <h1><a href="/"> DocCare</a></h1>
+        <h1 className="uppercase font-bold"><NavLink to="/"> DocCare</NavLink></h1>
       </div>
       <ul>
         {NAVLINKS.map(({ name, route }) => (
-          <li key={name}>
-            <a
-              href={route}
-              className="block py-2 px-5 text-gray-600 hover:text-white hover:bg-[#97BF0E] transition-colors duration-300"
-            >
-              {name}
-            </a>
-          </li>
+          <NavLink
+            key={name}
+            to={route}
+            className="block py-2 px-5 text-gray-600 hover:text-white hover:bg-[#97BF0E] transition-colors duration-500 uppercase font-bold"
+            style={({ isActive }) => ({ backgroundColor: isActive ? '#97bf0e' : '', color: isActive ? '#fff' : '' })}
+          >
+            {name}
+          </NavLink>
         ))}
       </ul>
     </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 export const NAVLINKS = [
   { name: 'Doctors', route: '/' },
-  { name: 'Reserve', route: '/reserve' },
-  { name: 'My Reservations', route: '/my-reservations' },
-  { name: 'Add Doctor', route: '/add-doctor' },
-  { name: 'Delete Doctor', route: '/delete-doctor' },
+  { name: 'Reserve', route: 'reserve' },
+  { name: 'My Reservations', route: 'my-reservations' },
+  { name: 'Add Doctor', route: 'add-doctor' },
+  { name: 'Delete Doctor', route: 'delete-doctor' },
 ];
 
 export const date = new Date();
