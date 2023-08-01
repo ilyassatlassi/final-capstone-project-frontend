d3f937a891c28d98e22a442407bb752813c975ef
SabiMantock <sabimantock@gmail.com>
2023-08-01 12:53:40 +0000

    fix: remove persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import storage from 'redux-persist/lib/storage';
 import userReducer from './slices/user';
 import doctorsReducer from './slices/doctors';
 import reservationsReducer from './slices/reservations';
 
-const persistConfig = {
-  key: 'user',
-  storage,
-};
-
-export const store = configureStore({
+export default configureStore({
   reducer: {
-    user: persistReducer(persistConfig, userReducer),
+    user: userReducer,
     doctors: doctorsReducer,
     reservations: reservationsReducer,
   },
   middleware: [thunk, createLogger()],
 });
-
-export const persistor = persistStore(store);
