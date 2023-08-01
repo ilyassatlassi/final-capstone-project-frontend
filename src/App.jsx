f98a942e879c7c307ec49d55fc40b0f4fb9be963
SabiMantock <sabimantock@gmail.com>
2023-08-01 09:39:21 +0000

    feat: Add routes for login and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import AppLayout from './layout/AppLayout';
 import ProtectedRoute from './components/ProtectedRoute';
 import DetailPage from './pages/DetailPage';
 import DoctorPage from './pages/DoctorPage';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <Routes>
-      <Route path="auth/signin" />
+      <Route path="/auth/signin" />
+      <Route path="/auth/register" element={<Signup />} />
       <Route path="/" element={<ProtectedRoute component={AppLayout} />}>
         <Route index element={<DetailPage />} />
         <Route path="/reserve" element={<DoctorPage />} />
