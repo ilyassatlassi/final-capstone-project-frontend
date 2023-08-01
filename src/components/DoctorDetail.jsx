62cfecbd3141e1fd90017db54719db32d4c89b40
ilyass atlassi <atlassiilyass@gmail.com>
2023-08-02 00:49:08 +0100

    Add reservation button

--- src/components/DoctorDetail.jsx
+++ src/components/DoctorDetail.jsx
@@ -18,11 +18,11 @@ function DoctorDetail() {
   }, [dispatch]);
 
   return (
-    <div className="p-6 w-full text-center">
+    <div className="p-6 w-full text-center ">
       <div className="flex flex-col">
         {/* <h3 className="uppercase font-bold">Doctor Details</h3> */}
 
-        <div className="flex mt-20">
+        <div className="lg:flex mt-20">
           <div className="flex justify-center lg:w-3/4">
             <img
               src="https://img.freepik.com/free-photo/portrait-smiling-young-woman-doctor-healthcare-medical-worker-pointing-fingers-left-showing-clini_1258-88108.jpg?w=996&t=st=1690911597~exp=1690912197~hmac=3a4f5f061838832744eb3e61e5469ab1395d27e15ea84787339d4a6f9f07fbeb"
@@ -30,7 +30,7 @@ function DoctorDetail() {
               className="w-[500px] h-[400px] bg-white"
             />
           </div>
-          <div className="lg:w-1/4">
+          <div className="lg:w-1/4 md:mt-2">
             <div className="uppercase font-bold text-right">{Doctor.name}</div>
             <div className="text-center mt-2">{Doctor.description}</div>
             <table className="table-auto w-full border-collapse mt-2">
@@ -50,29 +50,36 @@ function DoctorDetail() {
               </tbody>
             </table>
 
-            <div className="flex flex-row justify-around mt-6">
+            <div className="flex flex-row justify-around mt-10">
               <a
-                href="https://twitter.com"
+                href={Doctor.twitter}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaTwitter size={30} color="blue" />
+                <FaTwitter size={30}  />
               </a>
               <a
-                href="https://instagram.com"
+                href={Doctor.instagram}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaInstagram size={30} color="purple" />
+                <FaInstagram size={30} />
+                
               </a>
               <a
-                href="https://facebook.com"
+                href={Doctor.facebook}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaFacebook size={30} color="blue" />
+                <FaFacebook size={30} />
               </a>
             </div>
+            <button
+              type="submit"
+              className="w-auto lg:w-60  bg-[#96bf01] hover:bg-green-500 text-white rounded-full mt-10 py-2 font-bold"
+            >
+              Reservation
+            </button>
           </div>
         </div>
       </div>
