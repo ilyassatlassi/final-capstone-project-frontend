f9246a0849c73c5e301ce2781afc621f00feda6e
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-08-01 14:58:45 +0430

    Edit routes for login page in App.jsx file!

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import AppLayout from './layout/AppLayout';
 import ProtectedRoute from './components/ProtectedRoute';
 import DetailPage from './pages/DetailPage';
 import DoctorPage from './pages/DoctorPage';
+import LoginPage from './components/user/LoginPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="auth/signin" />
+      <Route path="/auth/sign_in" element={<LoginPage />} />
       <Route path="/" element={<ProtectedRoute component={AppLayout} />}>
         <Route index element={<DetailPage />} />
         <Route path="/reserve" element={<DoctorPage />} />
