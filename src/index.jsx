1dc22f58eafd52ad03b8682ecf937d06e9447482
SabiMantock <sabimantock@gmail.com>
2023-08-01 12:58:38 +0000

    remove prsistgate

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </BrowserRouter>,
 );
