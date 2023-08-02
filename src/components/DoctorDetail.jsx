cf1226609f0bd00b33ddc013fde1a6d1f6cd8962
ilyass atlassi <atlassiilyass@gmail.com>
2023-08-02 01:19:38 +0100

    Add IoIosArrowForward link

--- src/components/DoctorDetail.jsx
+++ src/components/DoctorDetail.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from 'react-router-dom';
 import { fetchDoctor } from "../redux/slices/doctors";
 import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
+import { IoIosArrowForward } from 'react-icons/io';
 
 function DoctorDetail() {
   const dispatch = useDispatch();
@@ -22,16 +23,16 @@ function DoctorDetail() {
       <div className="flex flex-col">
         {/* <h3 className="uppercase font-bold">Doctor Details</h3> */}
 
-        <div className="lg:flex mt-20">
+        <div className="lg:flex justify-around lg:mt-20">
           <div className="flex justify-center lg:w-3/4">
             <img
               src="https://img.freepik.com/free-photo/portrait-smiling-young-woman-doctor-healthcare-medical-worker-pointing-fingers-left-showing-clini_1258-88108.jpg?w=996&t=st=1690911597~exp=1690912197~hmac=3a4f5f061838832744eb3e61e5469ab1395d27e15ea84787339d4a6f9f07fbeb"
               alt={Doctor.name}
-              className="w-[500px] h-[400px] bg-white"
+              className="w-[500px] lg:h-[400px] bg-white"
             />
           </div>
           <div className="lg:w-1/4 md:mt-2">
-            <div className="uppercase font-bold text-right">{Doctor.name}</div>
+            <div className="uppercase font-bold lg:text-right">{Doctor.name}</div>
             <div className="text-center mt-2">{Doctor.description}</div>
             <table className="table-auto w-full border-collapse mt-2">
               <tbody>
@@ -74,9 +75,16 @@ function DoctorDetail() {
                 <FaFacebook size={30} />
               </a>
             </div>
+            <Link
+          to="/index"
+          className="flex  justify-center font-bold items-center gap-2 mt-10"
+        >
+          DISCOVER MORE DOCTORS
+          <IoIosArrowForward fill="black" />
+        </Link>
             <button
               type="submit"
-              className="w-auto lg:w-60  bg-[#96bf01] hover:bg-green-500 text-white rounded-full mt-10 py-2 font-bold"
+              className="w-auto lg:w-60  bg-[#96bf01] hover:bg-green-500 text-white rounded-full mt-10 p-2 font-bold"
             >
               Reservation
             </button>
