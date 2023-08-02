98a7733c9bade09d98c448f1b9d1322874884867
ilyass atlassi <atlassiilyass@gmail.com>
2023-08-03 00:43:50 +0100

    Add Home page test

--- src/__test__/homepage.test.js
+++ src/__test__/homepage.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Homepage from '../pages/homepage';
-import store from '../redux/store/index';
+import HomePage from '../pages/HomePage';
+import store from './redux/store';
 
 describe('Components testing', () => {
 	test('home component', () => {
-		const tree = render(<Provider store={store}><BrowserRouter><Routes><Route path="/" element={<Homepage />} /></Routes></BrowserRouter></Provider>);
+		const tree = render(<Provider store={store}><BrowserRouter><Routes><Route path="/" element={<HomePage />} /></Routes></BrowserRouter></Provider>);
 		expect(tree).toMatchSnapshot();
 	});
 });
