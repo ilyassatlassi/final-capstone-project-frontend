2603a290426ff045229e3a1df28819a9b97cd0fd
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-08-03 02:43:07 +0430

    Create table for the doctor's list!

--- src/pages/DeleteDoctorPage.jsx
+++ src/pages/DeleteDoctorPage.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
-// import DeleteDoctor from '../components/DeleteDoctor';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDoctors } from '../redux/slices/doctors';
+import DeleteDoctor from '../components/DeleteDoctor';
 
-const DeleteDoctorPage = () => (
-  <div>
-    this is delete 
-  </div>
-);
+const DeleteDoctorPage = () => {
+  const dispatch = useDispatch();
+  const { doctors } = useSelector((state) => state.doctors);
+
+  useEffect(() => {
+    dispatch(fetchDoctors());
+  }, [dispatch]);
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
+      <h1 className="text-3xl font-extrabold text-gray-900 mb-8">List of Doctors</h1>
+      <div className="bg-white overflow-hidden shadow-xl rounded-lg">
+        <div className="px-4 py-5 sm:p-6">
+          <div className="overflow-x-auto">
+            <table className="w-full table-auto divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    description
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Consultation Fee
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Hospital
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="sr-only">Delete</span>
+                  </th>
+                </tr>
+              </thead>
+
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DeleteDoctorPage;
